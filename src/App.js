04c8c0ed693b31ce9485540537b473607f8e7533
Ali Zain <vin.alizain@gmail.com>
2021-04-03 00:39:44 +0500

    TensorflowJs
    React.js and Tensorflow.js Basic Project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+// Import Packages
+import * as tf from "@tensorflow/tfjs";
+import * as speech from "@tensorflow-models/speech-commands";
+import { useState, useEffect } from "react";
 
 function App() {
+  // 1. Create model and action states
+  const [model, setModel] = useState(null);
+  const [action, setAction] = useState(null);
+  const [labels, setLabels] = useState(null);
+
+  // 2. Create Recognizer
+
+  const loadelModel = async () => {
+    const recognizer = await speech.create("BROWSER_FFT");
+    console.log("Model loaded");
+    await recognizer.ensureModelLoaded();
+    console.log(recognizer.wordLabels());
+    setModel(recognizer);
+    setLabels(recognizer.wordLabels());
+  };
+
+  useEffect(() => {
+    loadelModel();
+  }, []);
+
+  // 3. Listen for actions
+
+  function argMax(arr) {
+    return arr.map((x, i) => [x, i]).reduce((r, a) => (a[0] > r[0] ? a : r))[1];
+  }
+
+  const recognizeCommands = async () => {
+    console.log("Listening for commands");
+    model.listen(
+      (result) => {
+        console.log(result.spectrogram);
+        setAction(labels[argMax(Object.values(result.scores))]);
+      },
+      { includeSpectrogram: true, probabilityThreshold: 0.9 }
+    );
+    setTimeout(() => model.stopListening(), 10e5);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +52,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={recognizeCommands}>Command</button>
+        {action ? <div>{action}</div> : <div>No Action Detected</div>}
       </header>
     </div>
   );
